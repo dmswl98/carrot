b9789f704ea8355bb672101ba8a0c2f69c47662b
cje <chej098t@gmail.com>
2024-12-02 23:54:43 +0900

    chore: 프로젝트 세팅

---
+++ src/app/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+export const pretendard = localFont({
+  src: "./fonts/PretendardVariable.woff2",
+  variable: "--font-pretendard",
+  display: "swap",
+  preload: true,
+  fallback: [
+    "Pretendard Variable",
+    "Pretendard",
+    "-apple-system",
+    "BlinkMacSystemFont",
+    "system-ui",
+    "Roboto",
+    "Helvetica Neue",
+    "Segoe UI",
+    "Apple SD Gothic Neo",
+    "Noto Sans KR",
+    "Malgun Gothic",
+    "Apple Color Emoji",
+    "Segoe UI Emoji",
+    "Segoe UI Symbol",
+    "sans-serif",
+  ],
+});
+
+export const metadata: Metadata = {
+  title: "데이터로 똑똑하게, 캐롯",
+  description: "캐롯",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={`${pretendard.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div>🧑🏻‍🌾</div>;
+}
